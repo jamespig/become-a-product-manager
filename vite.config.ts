0489b468192aad54d12a722f5db52def8c67795b
jamespig <james7766646@gmail.com>
2026-06-20 10:59:01 +0800

    deploy: 部署至github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const githubPagesBase = '/become-a-product-manager/'
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? githubPagesBase : '/',
+  plugins: [tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
